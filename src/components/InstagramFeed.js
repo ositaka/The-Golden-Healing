7e88579261609923dd9d6c410fbcf524d23de76d
Nuno Marques <ositaka@gmail.com>
2020-01-25 11:12:43 +0000

    Update InstagramFeed.js

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -8,7 +8,7 @@ import './InstagramFeed.css'
 
 export default class InstagramFeed extends Component {
   static defaultProps = {
-    accessToken: '1353697840.1677ed0.5a1cbfbc18f84915aa0d9a0bd02bff5a',
+    accessToken: '247836798.1677ed0.c22b6c36b7fd41e9aff06637d6790afa',
     count: 20
   }
 
