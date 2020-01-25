f17858c4b33fa0035868bd9d6c6c7014672e55be
Nuno Marques <ositaka@gmail.com>
2020-01-25 11:45:26 +0000

    changing stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://instagram.com/thegoldenhealing/">@thegoldenhealing</a>
     </h2>
     <br />
     <InstagramFeed count="8" />
@@ -14,7 +14,7 @@ export default () => (
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="https://mediacreators.studio">Media Creators Studio</a>.
         </span>
       </div>
     </footer>
